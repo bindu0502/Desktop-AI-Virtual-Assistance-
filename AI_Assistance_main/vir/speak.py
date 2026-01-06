a8b89ac418208299666bec91142e0e3e81cfd492
bindu0502 <pbindusri0502@gmail.com>
2026-01-06 10:39:59 +0530

    Delete AI_Assistance_main directory

--- AI_Assistance_main/vir/speak.py
+++
@@ -1,9 +0,0 @@
-# pip install pyttsx3
-
-import pyttsx3
-def speak(text):
-    engine = pyttsx3.init()
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', rate-70)
-    engine.say(text)
-    engine.runAndWait()
